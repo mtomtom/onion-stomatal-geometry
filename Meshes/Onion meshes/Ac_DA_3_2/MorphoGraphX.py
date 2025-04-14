4186f60b51a6cb876bbe3fec96475d7c354582b2
Richard S. Smith <Richard.Smith@jic.ac.uk>
2025-04-14 08:08:27 +0100

    Added in the new, idealised meshes based on the confocal images

---
+++ Meshes/Onion meshes/Ac_DA_3_2/MorphoGraphX.py
@@ -0,0 +1,10 @@
+######## New MorphoGraphX session v2.0 r1-189: 2023-10-03 10:09:41
+Process.Misc__System__Save_View('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_3/Ac_DA_3_1/Ac_DA_3_1_mesh2_final.mgxv')
+Process.Stack__System__Open('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_3/Ac_DA_3_3/Ac_DA_3_3_2023_09_11__14_24_49_Out.tif', 'Main', '0', '')
+Process.Misc__System__Load_View('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_3/Ac_DA_3_2/Ac_DA_3_2_final.mgxv')
+######## New MorphoGraphX session v2.0 r1-189: 2023-10-03 14:28:21
+Process.Misc__System__Load_View('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_3/Ac_DA_3_2/Ac_DA_3_2_final.mgxv')
+Process.Stack__Canvas__Change_Voxel_Size('0.5', '0.5', '1.0')
+Process.Mesh__Creation__Marching_Cubes_3D('1', '0', '3', '0')
+######## New MorphoGraphX session v2.0 r1-189: 2023-10-11 11:18:21
+Process.Misc__System__Load_View('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_3/Ac_DA_3_2/Ac_DA_3_2_final.mgxv')
