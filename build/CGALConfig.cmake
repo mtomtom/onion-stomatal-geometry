f3f27935e43bd388edbb5029796df14e61f80b1e
Melissa Tomkins <melissa.tomkins@jic.ac.uk>
2025-10-30 16:29:48 +0000

    Removed more files that don't need to be in the repo

--- build/CGALConfig.cmake
+++
@@ -1,5 +0,0 @@
-# 
-# This file points to the CGALConfig.cmake for header-only CGAL.
-#
-
-include("/Users/tomkinsm/stomata-air-mattress/cgal/Installation/lib/cmake/CGAL/CGALConfig.cmake")
--- build/CGALConfigVersion.cmake
+++
@@ -1,5 +0,0 @@
-# 
-# This file points to the CGALConfigVersion.cmake for header-only CGAL.
-#
-
-include("/Users/tomkinsm/stomata-air-mattress/cgal/Installation/lib/cmake/CGAL/CGALConfigVersion.cmake")
