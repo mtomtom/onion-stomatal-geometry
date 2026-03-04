5259a31f862963d5273a7aa578e0325e150f3bc2
Melissa Tomkins <melissa.tomkins@jic.ac.uk>
2026-03-04 08:35:48 +0000

    Added the meshes for the anisotropy simulations

---
+++ Meshes/Onion meshes anisotropy/Ac_DA_1_2/MorphoGraphX.py
@@ -0,0 +1,16 @@
+######## New MorphoGraphX session v2.0 r1-189: 2023-09-28 11:38:09
+
+Process.Stack__System__Open('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_1/Ac_DA_1_3/Ac_DA_1_3_2023_09_11__13_21_44_Out_seg.tif', 'Main', '0', '')
+######## New MorphoGraphX session v2.0 r1-189: 2023-09-29 09:33:22
+
+Process.Stack__System__Open('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_1/Ac_DA_1_2/Ac_DA_1_2_2023_09_11__13_18_06_Out.tif', 'Main', '0', '')
+Process.Stack__System__Open('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_1/Ac_DA_1_2/Ac_DA_1_2_2023_09_11__13_18_06_Out.tif', 'Work', '0', '')
+Process.Mesh__System__Load('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_1/Ac_DA_1_2/Ac_DA_1_2_mesh.mgxm', 'no', 'no', '0')
+Process.Misc__System__Save_View('')
+######## New MorphoGraphX session v2.0 r1-189: 2023-09-29 09:40:50
+Process.Misc__System__Save_View('')
+Process.Mesh__Heat_Map__Heat_Map_Classic('Area', 'Geometry', '', 'Geometry', 'No', '0', '65535', 'Yes', 'No', 'None', 'No', 'Increasing', 'Ratio', '.001', '1.0')
+Process.Mesh__Heat_Map__Heat_Map_Classic('Area', 'Geometry', '', 'Geometry', 'No', '0', '65535', 'Yes', 'No', 'None', 'No', 'Increasing', 'Ratio', '.001', '1.0')
+Process.Misc__System__Save_View('/media/bitlockermount/Stomata project/Confocal data/Experiment 16 - Onion/Ac_DA_1/Ac_DA_1_2/Ac_DA_1_2_final.mgxv')
+Process.Mesh__Cell_Mesh__Convert_to_3D_Cell_Tissue('.01', '.01')
+Process.Mesh__Cell_Mesh__Convert_to_2D_Cell_Tissue('-1')
